201ff5183ecc16485ffcf73bb0955c115413226a
Jamelele <35628223+Jamelele@users.noreply.github.com>
2019-12-15 11:08:49 +0000

    Improve __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,1 +1,7 @@
-client_script 'client.lua'
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+description 'Exports current temperature based on time of day and current weather cycle'
+
+client_scripts {
+    'client.lua'
+} 
